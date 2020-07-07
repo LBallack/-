483369d4561a844c2fe0efe951cfc8c3774973da
test <1320293634@qq.com>
2020-07-07 22:25:44 +0800

    京津冀

---
+++ src/main/java/cn/tedu/RunSSM.java
@@ -0,0 +1,15 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class RunSSM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SpringApplication.run(RunSSM.class, args);
+	}
+
+}
