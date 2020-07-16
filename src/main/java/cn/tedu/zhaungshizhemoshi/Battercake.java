634155bc4acf8ec33c8fcdf67b5977358080aefa
test <1320293634@qq.com>
2020-07-16 22:52:35 +0800

    京津冀

---
+++ src/main/java/cn/tedu/zhaungshizhemoshi/Battercake.java
@@ -0,0 +1,12 @@
+package cn.tedu.zhaungshizhemoshi;
+
+public class Battercake {
+    protected String getDesc() {
+        return "煎饼果子";
+    }
+
+    protected int cost() {
+        return 8;
+    }
+
+}
