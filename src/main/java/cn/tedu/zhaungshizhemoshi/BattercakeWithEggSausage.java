0a79da3ac97167c4a1f008e07546763db8898591
test <1320293634@qq.com>
2020-08-31 22:22:27 +0800

    京津冀

--- src/main/java/cn/tedu/zhaungshizhemoshi/BattercakeWithEggSausage.java
+++ src/main/java/cn/tedu/zhaungshizhemoshi/BattercakeWithEggSausage.java
@@ -3,7 +3,7 @@ package cn.tedu.zhaungshizhemoshi;
 public class BattercakeWithEggSausage extends BattercakeWithEgg {
     @Override
     public String getDesc() {
-        return super.getDesc()+ " 加一根几把";
+        return super.getDesc()+ " 加一根香肠";
     }
 
     @Override
